da4b2a24a613b5711b6fd1bbcdce3c35a8238f58
maddiemasi <masim@clarkson.edu>
2016-12-12 16:58:25 -0500

    Add files via upload

---
+++ bxslider/slider.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	$('.bxslider').bxSlider({
+	  mode: 'fade',
+	  captions: true,
+	  auto: true,
+	  autoControls: false,
+	  pause: 5000
+	});
+});
+
+$(document).ready(function(){
+	$('.bxslider2').bxSlider({
+	  mode: 'fade',
+	  auto: false,
+	  autoControls: true,
+	  pause: 2000
+	});
+});
+
+$(document).ready(function(){
+	$('.bxslider3').bxSlider({
+	  mode: 'fade',
+	  auto: false,
+	  autoControls: true,
+	  pause: 2000
+	});
+});
+
+$(document).ready(function(){
+	$('.bxslider4').bxSlider({
+	  mode: 'fade',
+	  auto: false,
+	  autoControls: true,
+	  pause: 2000
+	});
+});
+
+$(document).ready(function(){
+	$('.bxslider5').bxSlider({
+	  mode: 'fade',
+	  auto: false,
+	  autoControls: true,
+	  pause: 2000
+	});
+});
